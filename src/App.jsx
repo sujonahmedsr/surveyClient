b8e9185e9b47dc7aa021def4f0466b6989356f01
Shofiqul Islam <110051682+sujonahmedsr@users.noreply.github.com>
2024-06-01 17:52:42 +0600

    first commit- tailwind css, react router, react install

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1>Vite + React</h1>
+      <h1 className="text-3xl font-bold font-inter">
+      Hello world!
+    </h1>
+      
+    </>
+  )
+}
+
+export default App
