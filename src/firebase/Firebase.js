f66c145b5ec1886bb5c9ad8e52e9793ffb8edac7
Shofiqul Islam <110051682+sujonahmedsr@users.noreply.github.com>
2024-06-03 12:11:10 +0600

    firebase key environment setup

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -3,15 +3,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-console.log(import.meta.env.VITE_apiKey);
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA4t7i2iFeN1zabayqNcbdgmyb3uHqodGo",
-  authDomain: "surveysky-c627d.firebaseapp.com",
-  projectId: "surveysky-c627d",
-  storageBucket: "surveysky-c627d.appspot.com",
-  messagingSenderId: "658506640033",
-  appId: "1:658506640033:web:7a1938dd54418c90061ee0"
+  apiKey:import.meta.env.VITE_apiKey,
+  authDomain:import.meta.env.VITE_authDomain,
+  projectId:import.meta.env.VITE_projectId,
+  storageBucket:import.meta.env.VITE_storageBucket,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId,
+  appId:import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
